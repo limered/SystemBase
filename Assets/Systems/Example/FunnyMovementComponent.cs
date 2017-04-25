93173e18c1bbbe5f482906ec4c89a13cd2ec5a7c
Emil Wasilewski <me@limered.de>
2017-04-25 18:45:59 +0200

    added funny movement example

---
+++ Assets/Systems/Example/FunnyMovementComponent.cs
@@ -0,0 +1,14 @@
+﻿using Assets.SystemBase;
+using UnityEngine;
+
+namespace Assets.Systems.Example
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class FunnyMovementComponent : GameComponent
+    {
+        public void Move(Vector3 direction)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Systems/Example/FunnyMovementConfigComponent.cs
@@ -0,0 +1,10 @@
+﻿using Assets.SystemBase;
+using UniRx;
+
+namespace Assets.Systems.Example
+{
+    public class FunnyMovementConfigComponent : GameComponent
+    {
+        public FloatReactiveProperty Speed = new FloatReactiveProperty(10);
+    }
+}
