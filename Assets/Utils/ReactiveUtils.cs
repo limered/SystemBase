24859ac0273c8c996df2468b97589d295c96097e
Emil Wasilewski <me@limered.de>
2017-04-25 18:45:25 +0200

    added utils

---
+++ Assets/Utils/ReactiveUtils.cs
@@ -0,0 +1,28 @@
+using System;
+using UniRx;
+using UnityEngine;
+
+namespace Assets.Utils
+{
+    public static class ReactiveUtils
+    {
+        public static IObservable<T> LogOnNext<T>(this IObservable<T> obs, string format = null)
+        {
+            return obs.Select(t => { Debug.Log(format == null ? (t == null ? null : t.ToString()) : string.Format(format, t)); return t; });
+        }
+
+        public static IObservable<T> LogError<T>(this IObservable<T> obs, Func<Exception, string> convertToString)
+        {
+            return obs.Do(x => { }, err =>
+            {
+                if (convertToString != null) Debug.LogError(convertToString(err));
+                else Debug.LogError(err.Message + "\n" + err.StackTrace);
+            });
+        }
+
+        public static IObservable<T> LogError<T>(this IObservable<T> obs)
+        {
+            return obs.Do(x => { }, Debug.LogException);
+        }
+    }
+}
